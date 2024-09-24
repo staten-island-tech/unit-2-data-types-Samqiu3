99f43f1af61c60d5e10d337e5738f417ee3345db
Samqiu3 <samq3@nycstudents.net>
2024-09-24 09:20:38 -0400

    9/24

--- GCF.py
+++ GCF.py
@@ -1,6 +1,8 @@
 numone=int(input("type a number"))
 numtwo=int(input("type another number"))
-for y in range(1,numone + 1):
+
+for y in range(1,min(numone, numtwo) + 1):
   if numtwo % y == 0 and numone % y == 0:
     gcf=y
-    print(gcf)
+    
+print(gcf)
